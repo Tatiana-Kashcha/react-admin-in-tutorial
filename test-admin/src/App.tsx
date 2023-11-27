dcba4a17a5563d7dd7c0097ad0654936a7e50ba8
Tatiana Kashcha <tkashcha@gmail.com>
2023-11-27 20:17:00 +0200

    add icons

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -8,21 +8,31 @@ import {
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 import { UserList } from "./users";
+import UserIcon from "@mui/icons-material/Group";
+import PostIcon from "@mui/icons-material/Book";
+import CommentIcon from "@mui/icons-material/Comment";
+import { Dashboard } from "./Dashboard";
 
 export const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+  <Admin
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    dashboard={Dashboard}
+  >
     <Resource
       name="posts"
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
+      icon={PostIcon}
     />
     <Resource
       name="comments"
       list={ListGuesser}
       edit={EditGuesser}
       show={ShowGuesser}
+      icon={CommentIcon}
     />
-    <Resource name="users" list={UserList} />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
